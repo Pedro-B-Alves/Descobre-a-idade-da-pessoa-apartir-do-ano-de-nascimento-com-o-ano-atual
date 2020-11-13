94d208757d88ca769f121d15d3bd1566265e6a79
Pedro Alves <pedro22carro@gmail.com>
2020-11-13 19:38:13 -0300

    Cálculo finalizado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exe._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o ano que você nasceu");
+            int ano=int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o ano atual");
+            int atualAno=int.Parse(Console.ReadLine());
+            int nascimento=atualAno-ano;
+            double semanas=nascimento*52.1426; //Um ano tem 52,1429 semanas
+            Console.WriteLine("Idade dessa pessoa é "+nascimento+" anos");            
+            Console.WriteLine("Essa idade convertida em semana é "+semanas+" semanas");
+        }
+    }
+}
